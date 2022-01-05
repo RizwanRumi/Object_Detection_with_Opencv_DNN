59a98e42368b880c8cb3b121add701196e4f0fb4
RizwanRumi <rizwan.cse33@gmail.com>
2022-01-05 21:55:33 +0100

    add network configuration files

---
+++ Config/Opencv_DNN.py
@@ -0,0 +1,20 @@
+import cv2 as cv
+
+class ModelConfiguration():
+
+    def __init__(self, config, model):
+        self.config = config
+        self.model = model
+
+    def modelConfig(self, dnn_name):
+        if dnn_name == "MASKRCNN":
+            network = cv.dnn.readNetFromTensorflow(self.model, self.config)
+        elif(dnn_name == "YOLOV4"):
+            network = cv.dnn.readNetFromDarknet(self.config, self.model)
+        else:
+            network = None
+
+        network.setPreferableBackend(cv.dnn.DNN_BACKEND_DEFAULT)
+        network.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)
+
+        return network
